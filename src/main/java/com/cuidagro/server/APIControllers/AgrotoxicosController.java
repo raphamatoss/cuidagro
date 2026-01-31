86ce786c7b7964b4b20487b69db08cdaa1e25f6b
Raphael Matos <raphaelmsgoncalves@gmail.com>
2026-01-31 18:00:23 -0300

    Adição do POST HTTP para persistir os agrotoxicos inseridos pelo agricultor

--- src/main/java/com/cuidagro/server/APIControllers/AgrotoxicosController.java
+++ src/main/java/com/cuidagro/server/APIControllers/AgrotoxicosController.java
@@ -1,12 +1,10 @@
 package com.cuidagro.server.APIControllers;
 
 import com.cuidagro.server.Agrotoxico;
+import com.cuidagro.server.AgrotoxicoForms;
 import com.cuidagro.server.DBCommunication.DBAgrotoxicos;
 import com.cuidagro.server.helpers.Serializacao;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -23,5 +21,10 @@ public class AgrotoxicosController {
     public String agrotoxicosPorClasse(@RequestParam("classe") String classe) {
         ArrayList<Agrotoxico> agrotoxicos = DBAgrotoxicos.getByClass(classe);
         return Serializacao.serializarArrayList(agrotoxicos);
+    }
+
+    @PostMapping(value="/forms")
+    public void persistirUsoDeAgrotoxicosPorUsuario(@RequestBody AgrotoxicoForms forms) {
+        DBAgrotoxicos.persistirForms(forms);
     }
 }
--- src/main/java/com/cuidagro/server/DBCommunication/DBAgrotoxicos.java
+++ src/main/java/com/cuidagro/server/DBCommunication/DBAgrotoxicos.java
@@ -1,12 +1,14 @@
 package com.cuidagro.server.DBCommunication;
 
 import com.cuidagro.server.Agrotoxico;
+import com.cuidagro.server.AgrotoxicoForms;
 import com.cuidagro.server.enums.ClasseAgrotoxico;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.io.PrintStream;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 
 public class DBAgrotoxicos {
@@ -28,7 +30,7 @@ public class DBAgrotoxicos {
             }
         }
         catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Erro ao estabelecer conexão com o banco de dados: " + e.getMessage());
         }
         throw new RuntimeException("Erro ao buscar agrotoxicos no banco de dados.");
     }
@@ -52,9 +54,47 @@ public class DBAgrotoxicos {
             }
         }
         catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Erro ao estabelecer conexão com o banco de dados: " + e.getMessage());
         }
         throw new RuntimeException("Erro ao buscar agrotoxicos no banco de dados.");
+    }
+
+    public static void persistirForms(AgrotoxicoForms forms) {
+        Connection con = null;
+        try {
+            con = DBConnection.getConnection();
+            String user_cpf = forms.getCPF();
+            LocalDateTime data = forms.getData();
+            ZoneId zone = ZoneId.of("America/Sao_Paulo");
+            Timestamp timestamp = Timestamp.from(data.atZone(zone).toInstant());
+            Agrotoxico[] agrotoxicos = forms.getAgrotoxicos();
+            con.setAutoCommit(false);
+            for (Agrotoxico a : agrotoxicos) {
+                String sql = "INSERT INTO mydb.agricultor_has_agrotoxico (agricultor_cpf, agrotoxico_nome, dataformulario)" +
+                        " VALUES (?, ?, ?)";
+                PreparedStatement preparedStatement = con.prepareStatement(sql);
+                preparedStatement.setString(1, user_cpf);
+                preparedStatement.setString(2, a.getNome());
+                preparedStatement.setTimestamp(3, timestamp);
+
+                preparedStatement.executeUpdate();
+            }
+            con.commit();
+            con.setAutoCommit(true);
+        }
+        catch (Exception e) {
+            if (con != null) {
+                try {
+                    con.rollback();
+                    System.out.println("Rollback executado!");
+                    con.setAutoCommit(true);
+                }
+                catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+            System.out.println("Erro ao estabelecer conexão com o banco de dados: " + e.getMessage());
+        }
     }
 
 }
