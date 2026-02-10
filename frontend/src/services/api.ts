ef69b98db6c8da07fbebd1e5de96c22fbae018f7
Artur Alencar <artur.alencar@dcomp.ufs.br>
2026-02-10 13:01:00 -0300

    config deploy vercel

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://3.19.127.120:9090',
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
     },
